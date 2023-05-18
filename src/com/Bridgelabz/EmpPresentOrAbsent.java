7e7e01f0971ab9a010f4ce6da4f7b3493dc9c258
minal1593 <minalsalunkhe15@gmail.com>
2023-05-18 12:01:16 +0530

    Added : Uc1-code to check employee present or absent

--- src/com/Bridgelabz/EmpPresentOrAbsent.java
+++ src/com/Bridgelabz/EmpPresentOrAbsent.java
@@ -3,7 +3,8 @@ package com.Bridgelabz;
 public class EmpPresentOrAbsent {
     public static void main(String[] args) {
         int empcheck=(int)((Math.random()*10)%2);
-        if(empcheck==1){
+        if(empcheck==1)
+        {
             System.out.println("Employee is present");
         }
         else {
